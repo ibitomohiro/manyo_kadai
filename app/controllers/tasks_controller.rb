3f7918cf8ab608d0e8b0b3a7aaf55deef3e17c12
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-11-19 22:50:29 +0900

    Delete まで作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -8,16 +9,48 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:notice] = "Taskの編集としました！"
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
   def create
-    Task.create(task_params)
+    @task = Task.create(task_params)
+    if @task.save
     # binding.pry
+    flash[:notice] = 'Taskの投稿に成功しました！'
     redirect_to new_task_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:notice] = "Taskを削除しました！"
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
