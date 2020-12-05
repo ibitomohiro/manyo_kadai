8aa901b0ab07a188c14cba2d2e0f01713f06013b
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-12-05 18:44:33 +0900

    add: enumでpriorityを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,5 @@ class Task < ApplicationRecord
   scope :over, -> { order(dead_line: :asc)}
   scope :search_title, ->  (title) {where('title LIKE ?', "%#{ title }%")}
   scope :search_status, -> (status) {where(status: status)}
+  enum priority: { 高: 0, 中:1, 低:2 }
 end
