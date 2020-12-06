c6c80bea5ac1b130b512dab869b9f86461d9f1b2
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-12-06 15:59:38 +0900

    add: priority のscopeの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,5 +7,6 @@ class Task < ApplicationRecord
   scope :over, -> { order(dead_line: :asc)}
   scope :search_title, ->  (title) {where('title LIKE ?', "%#{ title }%")}
   scope :search_status, -> (status) {where(status: status)}
+  scope :search_priority, -> { order(priority: :desc) }
   enum priority: { 高: 0, 中:1, 低:2 }
 end
