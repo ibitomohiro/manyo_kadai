6da79c814671a4f81f9ea4bca8bb1b25b868fdf7
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-12-13 21:06:20 +0900

    add: Session　view ぺーじの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'tasks#index'
 end
