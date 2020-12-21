f9ecf2f1b55433d6e57b34bb5ea4e2f80071d9aa
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-12-21 19:32:05 +0900

    add: adminユーザーのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'tasks#index'
+  namespace :admin do
+    resources :users
+  end
 end
