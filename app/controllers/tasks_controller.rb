5b204b16f2de8932fbfe6b8786bdd44cdf3c3871
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-01-11 20:51:27 +0900

    change: formでlabelにチェックを入れる機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -62,7 +62,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content, :dead_line, :status, :priority)
+    params.require(:task).permit(:title, :content, :dead_line, :status, :priority, label_ids: [])
   end
 
   def set_task
